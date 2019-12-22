329880505dda38c77d16142ec0ea3da44032f910
appdevgenie <appdevgenie@gmail.com>
2019-12-22 10:13:49 +0200

    update mobile ui

--- mobile/src/main/java/com/appdevgenie/mobile/MainActivity.java
+++ mobile/src/main/java/com/appdevgenie/mobile/MainActivity.java
@@ -1,8 +1,11 @@
 package com.appdevgenie.mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+
+        ConstraintLayout clRoomTemp = findViewById(R.id.includeRoomTemp);
+        TextView tvRoomTitle = clRoomTemp.findViewById(R.id.tvTempTitle);
+        tvRoomTitle.setText("Room");
+        ProgressBar pbRoom = clRoomTemp.findViewById(R.id.pbTemperature);
+        pbRoom.setProgress(30);
+
+
+        ConstraintLayout clSystemTemp = findViewById(R.id.includeSystemTemp);
+        TextView tvSystemTitle = clSystemTemp.findViewById(R.id.tvTempTitle);
+        tvSystemTitle.setText("System");
+        ProgressBar pbSystem = clSystemTemp.findViewById(R.id.pbTemperature);
+        pbSystem.setProgress(45);
+
+
+        ConstraintLayout clWaterTemp = findViewById(R.id.includeWaterTemp);
+        TextView tvWaterTemp = clWaterTemp.findViewById(R.id.tvTempTitle);
+        tvWaterTemp.setText("Water");
+        ProgressBar pbWater = clWaterTemp.findViewById(R.id.pbTemperature);
+        pbWater.setProgress(20);
     }
 }
